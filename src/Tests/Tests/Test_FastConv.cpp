4643b17228cbfcc043ec1c4e4e1a6075fa2e2cde
Liang_Tang <ltangmt@gmail.com>
2016-03-01 20:54:54 -0500

    Run test for fast convolution

--- src/Tests/Tests/Test_FastConv.cpp
+++ src/Tests/Tests/Test_FastConv.cpp
@@ -39,84 +39,85 @@ SUITE(FastConv)
     };
 
 //    only for testing the basic convolution equation works
-//    TEST_FIXTURE(FastConvData, convTest) {
-//        float impulse[6]   = { 1, 2, 3, 4, 5, 6},
-//              input[3] = { 7, 8, 9},
-//              output[8]  = { 7, 22, 46, 70, 94, 118, 93, 54};
-//        float *procOut = new float[8];
-//
-//        m_pCFastConv->init( impulse, 6, 5, CFastConv::kTimeDomain );
-//        m_pCFastConv->process( input, procOut, 3 );
-//
-//        CHECK_ARRAY_EQUAL( output, procOut, 8 );
-//    }
+    TEST_FIXTURE(FastConvData, convTest) {
+        float impulse[6]   = { 1, 2, 3, 4, 5, 6},
+              input[3] = { 7, 8, 9},
+              output[8]  = { 7, 22, 46, 70, 94, 118, 93, 54};
+        float *procOut = new float[8];
 
-//    TEST_FIXTURE(FastConvData, inputBufferStorageTest) {
-//        float input[18],
-//              impulse[4] = { 1, 1, 1, 1}; 
-//        float *procOut = new float[21];
-//        int blockLen   = 3;
-//
-//        for( int sample =0; sample<18; sample++) {
-//            input[sample] = sample*1.0f; 
-//        }
-//
-//        m_pCFastConv->init( impulse, 4, blockLen, CFastConv::kTimeDomain );
-//        for( int block = 0; block<6; block++ ) {
-//            m_pCFastConv->process( &input[block*blockLen], &procOut[block*blockLen], blockLen );
-//        }
-//    }
+        m_pCFastConv->init( impulse, 6, 5, CFastConv::kTimeDomain );
+        m_pCFastConv->process( input, procOut, 3 );
 
-//    TEST_FIXTURE
+        CHECK_ARRAY_EQUAL( output, procOut, 8 );
+    }
+
+    TEST_FIXTURE(FastConvData, inputBufferStorageTest) {
+        float input[18],
+              impulse[4] = { 1, 1, 1, 1}; 
+        float *procOut = new float[21];
+        int blockLen   = 3;
+
+        for( int sample =0; sample<18; sample++) {
+            input[sample] = sample*1.0f; 
+        }
+
+        m_pCFastConv->init( impulse, 4, blockLen, CFastConv::kTimeDomain );
+        for( int block = 0; block<6; block++ ) {
+            m_pCFastConv->process( &input[block*blockLen], &procOut[block*blockLen], blockLen );
+        }
+        delete procOut; procOut = 0;
+    }
+
+
     
-//    //Question3.1
-//    TEST_FIXTURE(FastConvData, IrTest)
-//    {
-//        //create a impulse response of 51 seconds with random index of 1
-//        int iSampleRate = 44100;
-//        int iIRLengthInSec = 30;
-//        int iIRLengthInSample = iSampleRate * iIRLengthInSec;
-//        float* pfRandomIR = new float[iIRLengthInSample];
-//        float decEnv = 1;
-//        
-//        for (int sample = 0; sample < iIRLengthInSample; sample++) {
-//            pfRandomIR[sample] =   decEnv - 0.1f * (static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
-//            if (sample % 10000 == 0) {
-//                //                std::cout<<pfRandomIR[sample]<<"  "<<decEnv<<std::endl;
-//            }
-//            //            decEnv = decEnv-1/iIRLengthInSample;
-//        }
-//        
-//        // create a input signal: delayed impulse (5 samples)
-//        int iInputLength = 10000;
-//        float* pfInputSignal = new float[iInputLength];
-//        float* pfOutputSignal = new float[iIRLengthInSample + iInputLength - 1];
-//        
-//        for( int sample = 0; sample<iInputLength; sample++) {
-//            pfInputSignal[sample] = 0.0f;
-//        }
-//        pfInputSignal[5] = 1.f;
-//        
-//        // create the reference signal: shifted IR (5 samples)
-//        float* pfReference = new float[iIRLengthInSample];
-//        for (int sample = 0; sample < 5; sample++) {
-//            pfReference[sample] = 0.f;
-//        }
-//        for (int sample = 5; sample < iIRLengthInSample; sample++) {
-//            pfReference[sample] = pfRandomIR[sample - 5];
-//        }
-//
-//        // identity test using process function
-//        m_pCFastConv->init(pfRandomIR, iIRLengthInSample, 4096, CFastConv::kTimeDomain);
-//        m_pCFastConv->process(pfInputSignal, pfOutputSignal, iInputLength);
-//        
-//        CHECK_ARRAY_EQUAL( pfReference, pfOutputSignal, iIRLengthInSample );
-//        
-//        delete pfRandomIR; pfRandomIR = 0;
-//        delete pfInputSignal; pfInputSignal = 0;
-//        delete pfOutputSignal; pfOutputSignal = 0;
-//        delete pfReference; pfReference = 0;
-//    }
+    //Question3.1
+    TEST_FIXTURE(FastConvData, IrTest)
+    {
+        //create a impulse response of 51 seconds with random index of 1
+        int iSampleRate = 44100;
+        int iIRLengthInSec = 30;
+        int iIRLengthInSample = iSampleRate * iIRLengthInSec;
+        float* pfRandomIR = new float[iIRLengthInSample];
+        float decEnv = 1;
+        
+        for (int sample = 0; sample < iIRLengthInSample; sample++) {
+            pfRandomIR[sample] =   decEnv - 0.1f * (static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
+            if (sample % 10000 == 0) {
+                //                std::cout<<pfRandomIR[sample]<<"  "<<decEnv<<std::endl;
+            }
+            //            decEnv = decEnv-1/iIRLengthInSample;
+        }
+        
+        // create a input signal: delayed impulse (5 samples)
+        int iInputLength = 10000;
+        float* pfInputSignal = new float[iInputLength];
+        float* pfOutputSignal = new float[iIRLengthInSample + iInputLength - 1];
+        
+        for( int sample = 0; sample<iInputLength; sample++) {
+            pfInputSignal[sample] = 0.0f;
+        }
+        pfInputSignal[5] = 1.f;
+        
+        // create the reference signal: shifted IR (5 samples)
+        float* pfReference = new float[iIRLengthInSample];
+        for (int sample = 0; sample < 5; sample++) {
+            pfReference[sample] = 0.f;
+        }
+        for (int sample = 5; sample < iIRLengthInSample; sample++) {
+            pfReference[sample] = pfRandomIR[sample - 5];
+        }
+
+        // identity test using process function
+        m_pCFastConv->init(pfRandomIR, iIRLengthInSample, 4096, CFastConv::kTimeDomain);
+        m_pCFastConv->process(pfInputSignal, pfOutputSignal, iInputLength);
+        
+        CHECK_ARRAY_EQUAL( pfReference, pfOutputSignal, iIRLengthInSample );
+        
+        delete pfRandomIR; pfRandomIR = 0;
+        delete pfInputSignal; pfInputSignal = 0;
+        delete pfOutputSignal; pfOutputSignal = 0;
+        delete pfReference; pfReference = 0;
+    }
     
     //Question3.2
     TEST_FIXTURE(FastConvData, InputBlockLengthTest)
